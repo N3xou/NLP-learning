5b8f6bf24756e3939ec686bfa90a8919993f11e1
Yami <santox25@gmail.com>
2024-10-07 20:10:12 +0200

    Count vector

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer
 
 %matplotlib inline
 sns.set_style("whitegrid")
 plt.style.use("fivethirtyeight")
+
+simple_train = ['call you tonight', 'Call me a cab', 'Please call me... PLEASE!']
+
+vect = CountVectorizer()
+
+# learn the 'vocabulary' of the training data (occurs in-place)
+vect.fit(simple_train)
+
+# examine the fitted vocabulary
+vect.get_feature_names_out()
