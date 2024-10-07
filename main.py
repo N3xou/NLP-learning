61ba82d545fb536ad1feeef8b498471416d5dc77
Yami <santox25@gmail.com>
2024-10-07 20:14:07 +0200

    python adj

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.feature_extraction.text import CountVectorizer
 
-%matplotlib inline
+
 sns.set_style("whitegrid")
 plt.style.use("fivethirtyeight")
 
@@ -12,11 +12,9 @@ simple_train = ['call you tonight', 'Call me a cab', 'Please call me... PLEASE!'
 
 vect = CountVectorizer()
 
-# learn the 'vocabulary' of the training data (occurs in-place)
 vect.fit(simple_train)
 
-# examine the fitted vocabulary
-vect.get_feature_names_out()
+print(vect.get_feature_names_out())
 
 simple_train_dtm = vect.transform(simple_train)
 simple_train_dtm
@@ -25,6 +23,5 @@ simple_train_dtm.toarray()
 pd.DataFrame(simple_train_dtm.toarray(), columns=vect.get_feature_names())
 
 print(type(simple_train_dtm))
-
 
 print(simple_train_dtm)
