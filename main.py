f4274491d86d431f1accf3407e4bb442c4dd6085
Yami <santox25@gmail.com>
2024-10-14 00:30:13 +0200

    txt pre proces

--- main.py
+++ main.py
@@ -66,3 +66,24 @@ sms[sms.label=='ham'].describe()
 sms[sms.label=='spam'].describe()
 
 sms[sms.message_len == 910].message.iloc[0]
+
+import string
+from nltk.corpus import stopwords
+
+
+def text_process(mess):
+    """
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Returns a list of the cleaned text
+    """
+    STOPWORDS = stopwords.words('english') + ['u', 'ü', 'ur', '4', '2', 'im', 'dont', 'doin', 'ure']
+    # Check characters to see if they are in punctuation
+    nopunc = [char for char in mess if char not in string.punctuation]
+
+    # Join the characters again to form the string.
+    nopunc = ''.join(nopunc)
+
+    # Now just remove any stopwords
+    return ' '.join([word for word in nopunc.split() if word.lower() not in STOPWORDS])
